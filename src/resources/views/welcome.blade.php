b88eb82c3fdbb1c10cf95cd8abcbcd211a51076a
sneko <superneko160@users.noreply.github.com>
2026-01-08 11:11:35 +0900

    feat: simple project -> laravel project

---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GCEデプロイテスト</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1 class="text-xl font-bold text-green-700">GCEデプロイテスト</h1>
+    <p><?=date('Y-m-d H:i:s')?></p>
+</body>
+</html>
